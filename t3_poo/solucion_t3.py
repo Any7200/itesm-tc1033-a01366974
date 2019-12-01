e7daa24557b6b949de243e28be6bdae0f79c70bf
Ana Laura Rodríguez <anarj14@hotmail.com>
2019-12-01 13:38:42 -0600

    Carga de archivos iniciales en rama t3_poo

---
+++ t3_poo/solucion_t3.py
@@ -0,0 +1,9 @@
+#A01366974 ANA LAURA RODRIGUEZ JURADO
+#-*- coding:utf-8 -*-
+import math
+from airportDP import *
+
+if __name__ == '__main__':
+	empty_airport = Airport()
+	populate_airport = AirportAD()
+	print(populate_airport.read_pilots_file()) #Imprime todo el diccionaro de objetos
